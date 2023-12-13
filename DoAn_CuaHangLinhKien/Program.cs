c965d3323bcb292f894b0cb1c7adbadf51477788
ThuanDuong <duongdoanthuan2003@gmail.com>
2023-12-14 00:10:42 +0700

    fix4

--- DoAn_CuaHangLinhKien/Program.cs
+++ DoAn_CuaHangLinhKien/Program.cs
@@ -16,7 +16,7 @@ namespace DoAn_CuaHangLinhKien
             Application.SetCompatibleTextRenderingDefault(false);
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
 
-            Application.Run(new frmManage());
+            Application.Run(new frmPhieuNhap());
         }
     }
 }
