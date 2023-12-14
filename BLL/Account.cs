442141dc9ab22546b4003d484a5ccdd8d8dcf91a
ThuanDuong <duongdoanthuan2003@gmail.com>
2023-12-14 21:09:34 +0700

    Add Account Class

---
+++ BLL/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DTO
+{
+    public class Account
+    {
+        public string AccountID { get; set; }
+        public int StaffID { get; set; }
+        public string Password { get; set; }
+        public int TypeID { get; set; }
+
+        public Account(string accountID, string password, int typeID, int staffID = null)
+        {
+            this.AccountID = accountID;
+            this.Password = password;
+            this.TypeID = typeID;
+            this.StaffID = staffID;
+        }
+        public Account(DataRow row)
+        {
+            this.AccountID = (string)row["MaTaiKhoan"];
+            this.Password = (string)row["MatKhau"];
+            this.TypeID = (int)row["MaLoaiTK "];
+            // Có thể lỗi
+            this.StaffID = (int)row["MaNhanVien"];
+        }
+    }
+}
---
+++ BLL/AccountType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class AccountType
+    {
+        public AccountType(int typeID, string typeName)
+        {
+            this.TypeID = typeID;
+            this.TypeName = typeName;
+        }
+
+        public AccountType(DataRow row)
+        {
+            this.TypeID = (int)row["MaLoaiTK"];
+            this.TypeName = (string)row["TenLoaiTK"]
+        }
+
+        public int TypeID { get; set; }
+        public string TypeName { get; set; }
+    }
+}
